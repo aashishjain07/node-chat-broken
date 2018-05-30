57460a6c9fb4dd5d8fbd5f46328d5dc801e6b44e
Ashish Jain <Ashish.jain8847@gmail.com>
2018-05-30 16:56:18 +0530

    Add newMessage and create message event

--- server/server.js
+++ server/server.js
@@ -14,7 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+// socket.emit('newEmail', {
+//   from: 'mike@example.com',
+//   text: 'Hey what is going on',
+//   createdAt : 123
+// });
 
+socket.emit('newMessage', {
+  from: 'Ashish',
+  text: 'See you later',
+  createdAt: '123123'
+})
+
+socket.on('createMessage', (message) => {
+  console.log('createMessage', message);
+});
+
+// socket.on('createEmail', (newEmail) => {
+//   console.log('createEmail', newEmail);
+// });
+//
 socket.on('disconnect', () => {
   console.log('User was disonnected');
 });
